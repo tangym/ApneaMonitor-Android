f74ad5790df74cdf1ddd16835dba9b9f7f85aa53
tangym <yut44@psu.edu>
2020-07-07 13:44:52 -0400

    add logging os and package information

--- watch/src/main/java/com/example/watch/SystemInformationLogger.java
+++ watch/src/main/java/com/example/watch/SystemInformationLogger.java
@@ -1,16 +1,23 @@
 package com.example.watch;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.Environment;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.OnLifecycleEvent;
 
 import java.io.*;
 import java.text.SimpleDateFormat;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Locale;
+import java.util.Properties;
 
 public class SystemInformationLogger implements LifecycleObserver {
     public static final String TAG = "watch:SystemInformationLogger";
@@ -28,6 +35,9 @@ public class SystemInformationLogger implements LifecycleObserver {
     public void run() {
         File outputFile = new File(outputDirectory, "sensors.csv");
         logSensorInfo(outputFile);
+
+        outputFile = new File(outputDirectory, "os.txt");
+        logOSInfo(outputFile);
     }
 
     public void logSensorInfo(File outputFile) {
@@ -56,4 +66,45 @@ public class SystemInformationLogger implements LifecycleObserver {
         }
     }
 
+    public void logOSInfo(File outputFile) {
+        String message = "";
+        try {
+            // TODO: add more information
+            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(
+                    context.getPackageName(), PackageManager.GET_META_DATA);
+            message += "Package name: " + context.getPackageName();
+            message += "\nVersion name: " + packageInfo.versionName;
+            message += "\nVersion code: " + packageInfo.versionCode;
+        } catch ( PackageManager.NameNotFoundException nnfe) {
+            nnfe.printStackTrace();
+        }
+
+        message += "\nOS version: " + System.getProperty("os.version") + " (" + Build.VERSION.INCREMENTAL + ")";
+        message += "\nAPI level: " + Build.VERSION.SDK;
+        message += "\nDevice: " + Build.DEVICE;
+        message += "\nModel: " + Build.MODEL;
+        message += "\nProduct: " + Build.PRODUCT;
+        message += "\nManufacturer: " + Build.MANUFACTURER;
+        message += "\nTags: " + Build.TAGS;
+
+        message += "\nScreen: " + ((Activity) context).getWindowManager().getDefaultDisplay().getWidth() + " x "
+                + ((Activity) context).getWindowManager().getDefaultDisplay().getHeight();
+        message += "\nSD card state: " + Environment.getExternalStorageState();
+
+        Properties properties = System.getProperties();
+        Enumeration keys = properties.keys();
+        String key = "";
+        while (keys.hasMoreElements()) {
+            key = (String) keys.nextElement();
+            message += "\n > " + key + " = " + (String) properties.get(key);
+        }
+
+        try {
+            FileWriter writer = new FileWriter(outputFile);
+            writer.write(message);
+            writer.close();
+        } catch ( IOException ioe ) {
+            ioe.printStackTrace();
+        }
+    }
 }
